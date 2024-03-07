ce3f15f41eadde819832dc6ed36eb099b8c1fd6c
Zyno16 <135590226+Zyno16@users.noreply.github.com>
2024-03-07 17:18:03 +0000

    Add files via upload

---
+++ aptitude.py
@@ -0,0 +1,13 @@
+c =int(input("enter the number of colunm:"))
+l=int(input("enter the number of colunm:"))
+for i in range(1,l+1):
+    for j in range(1,c+1):
+        print(i,end ="")
+    print(" ") 
+
+
+
+
+
+
+
